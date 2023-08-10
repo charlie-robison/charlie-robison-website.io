48d813ca152baaf27958eea7077b3b2379acc940
charlie-robison <charlierobison480@gmail.com>
2023-08-09 14:22:35 -1000

    Created the home page and added it to the app.js.

---
+++ my-app/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import { Container } from 'react-bootstrap';
+
+/** The home page of the application. */
+export const Home = () => {
+  return (
+    <Container>
+      <h1>Charlie Robison</h1>
+    </Container>
+  );
+};
