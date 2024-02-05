568e5482bd41e54dd77d0758e3ce593f302c36fd
charlie-robison <charlierobison480@gmail.com>
2024-02-04 17:13:39 -1000

    Created links to pages.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
 import { Home } from './pages/Home';
 import { Header } from './components/Header';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Home />
+    <Router>
+      <div className="d-flex flex-column min-vh-100">
+        <Header />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/skills" element={<Home />} />
+        <Route exact path="/careers" element={<Home />} />
+        <Route exact path="/projects" element={<Home />} />
+      </Routes>
     </div>
+    </Router>
   );
 }
 
--- my-app/src/components/Header.jsx
+++ my-app/src/components/Header.jsx
@@ -5,12 +5,12 @@ export const Header = () => {
   return (
     <Navbar bg="dark" data-bs-theme="dark">
       <Container>
-        <Navbar.Brand href="#home">Charlie Robison</Navbar.Brand>
+        <Navbar.Brand to="/">Charlie Robison</Navbar.Brand>
         <Nav className="me-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#skills">Skills</Nav.Link>
-          <Nav.Link href="#careers">Careers</Nav.Link>
-          <Nav.Link href="#projects">Projects</Nav.Link>
+          <Nav.Link to="/" key="home">Home</Nav.Link>
+          <Nav.Link to="/skills" key="skills">Skills</Nav.Link>
+          <Nav.Link to="/careers" key="careers">Careers</Nav.Link>
+          <Nav.Link to="/projects" key="projects">Projects</Nav.Link>
         </Nav>
       </Container>
     </Navbar>
